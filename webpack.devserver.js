83d64093b6b9a5ef916d9297ed7843586b5b98f0
cloudle <lehaoson@gmail.com>
2018-01-13 12:06:34 +0700

    Set CORS policy to allow hot-reload from localhost:3005..

--- webpack.devserver.js
+++ webpack.devserver.js
@@ -16,6 +16,11 @@ new WebpackDevServer(compiler, {
 	contentBase: 'web',
 	hot: true,
 	historyApiFallback: true,
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+		'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+	},
 	stats: {
 		assets:         false,
 		colors:         true,
