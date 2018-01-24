561e6c7d68a9eeb9d342e94b5d5e98def7fd11b7
cloudle <lehaoson@gmail.com>
2018-01-24 21:21:16 +0700

    markdownSource refactor using nested folder structure..

--- src/server/markdownSource.js
+++ src/server/markdownSource.js
@@ -3,16 +3,16 @@ import { join as joinPath } from 'path';
 
 const fileCache = {};
 
-export function getMarkdown(path) {
+export function getMarkdown(group, id) {
 	return new Promise((resolve, reject) => {
 		const existingCache = fileCache[reject];
 
 		if (existingCache) {
 			resolve(existingCache);
 		} else {
-			const fileLocation = joinPath(__dirname, '../markdowns', path);
+			const fileLocation = joinPath(__dirname, '../markdowns', group, `${id}.md`);
 			readFile(fileLocation, (error, data) => {
-				if (error) reject(error);
+				if (error) resolve(`## Oops!!!\nCouldn't find **${group}/${id}** documentation, it may been removed or temporary unavailable to access, try again latter!`);
 				else resolve(data.toString());
 			});
 		}
